2b3148b396e1d2fd11d7013f33c8aa328415c37c
Eric <54091924+EricHsia7@users.noreply.github.com>
2025-06-06 11:02:39 +0800

    update

--- src/lib/default-colors.ts
+++ src/lib/default-colors.ts
@@ -1,7 +1,7 @@
 export function getDefaultValue(property: string): string | false {
   const defaultValues: { [property: string]: string } = {
-    'color': '#000000',
-    'background-color': 'rgba(0, 0, 0, 0)'
+    color: '#000000'
+    /* 'background-color': 'rgba(0, 0, 0, 0)'*/
   };
 
   if (defaultValues.hasOwnProperty(property)) {
