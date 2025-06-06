3603ccbfa4c2a3c48259ad5b2b489c600cf36944
Eric <54091924+EricHsia7@users.noreply.github.com>
2025-06-06 17:41:59 +0800

    update

--- src/lib/parse-color.ts
+++ src/lib/parse-color.ts
@@ -310,7 +310,7 @@ export function invertParsedColor(color: ParsedColor): ParsedColor {
       }
 
       const newHue = originalHue;
-      const newSaturation = 1 - originalSaturation;
+      const newSaturation = originalSaturation;
       const newValue = Math.max(1 - originalValue, 0.08);
 
       const i = Math.floor(newHue * 6);
