b05f195d9c59938489cb7eee2fbd1f5b91b5d605
Eric <54091924+EricHsia7@users.noreply.github.com>
2025-06-06 12:21:26 +0800

    update

--- src/lib/styles.ts
+++ src/lib/styles.ts
@@ -12,7 +12,7 @@ export function getStyles() {
       'h1, h2, h3, h4, h5, h6': {
         color: '#1a1a1a'
       },
-      'p': {
+      'p, span, div': {
         color: '#2c2c2c'
       },
       'strong': {
