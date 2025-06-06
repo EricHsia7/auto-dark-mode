37eaa1c0f5599a69c7132226020a2b1164532490
Eric <54091924+EricHsia7@users.noreply.github.com>
2025-06-06 18:50:38 +0800

    update

--- src/initialize.ts
+++ src/initialize.ts
@@ -14,24 +14,26 @@ export function initialize(): void {
   );
   document.documentElement.appendChild(mask);
 
-  // Extract styles
-  const styles = getStyles();
+  setTimeout(function () {
+    // Extract styles
+    const styles = getStyles();
 
-  // Invert styles
-  const invertedStyles = invertStyles(styles);
+    // Invert styles
+    const invertedStyles = invertStyles(styles);
 
-  // Styles String
-  const strings = stylesToStrings(invertedStyles, false);
+    // Styles String
+    const strings = stylesToStrings(invertedStyles, false);
 
-  // Inject stylesheets
-  const fragment = new DocumentFragment();
-  for (const string of strings) {
-    const styleSheet = document.createElement('style');
-    styleSheet.textContent = string;
-    fragment.appendChild(styleSheet);
-  }
-  document.documentElement.appendChild(fragment);
+    // Inject stylesheets
+    const fragment = new DocumentFragment();
+    for (const string of strings) {
+      const styleSheet = document.createElement('style');
+      styleSheet.textContent = string;
+      fragment.appendChild(styleSheet);
+    }
+    document.documentElement.appendChild(fragment);
 
-  // Remove mask
-  document.querySelector('.auto_dark_mode_mask')?.classList.add('auto_dark_mode_fade_out');
+    // Remove mask
+    document.querySelector('.auto_dark_mode_mask')?.classList.add('auto_dark_mode_fade_out');
+  }, 1000);
 }
