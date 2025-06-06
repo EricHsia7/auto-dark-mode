5674b6ca0d7dcb46e5072ed8a93f758e190a6e20
Eric <54091924+EricHsia7@users.noreply.github.com>
2025-06-06 15:55:37 +0800

    update

--- src/initialize.ts
+++ src/initialize.ts
@@ -1,21 +1,23 @@
 import { getStyles, invertStyles, stylesToStrings } from './lib/styles';
 
 export function initialize(): void {
-  // Extract styles
-  const styles = getStyles();
+  setTimeout(function () {
+    // Extract styles
+    const styles = getStyles();
 
-  // Invert styles
-  const invertedStyles = invertStyles(styles);
+    // Invert styles
+    const invertedStyles = invertStyles(styles);
 
-  // Styles String
-  const strings = stylesToStrings(invertedStyles);
+    // Styles String
+    const strings = stylesToStrings(invertedStyles);
 
-  // Inject stylesheets
-  const fragment = new DocumentFragment();
-  for (const string of strings) {
-    const styleSheet = document.createElement('style');
-    styleSheet.textContent = string;
-    fragment.appendChild(styleSheet);
-  }
-  document.documentElement.appendChild(fragment);
+    // Inject stylesheets
+    const fragment = new DocumentFragment();
+    for (const string of strings) {
+      const styleSheet = document.createElement('style');
+      styleSheet.textContent = string;
+      fragment.appendChild(styleSheet);
+    }
+    document.documentElement.appendChild(fragment);
+  }, 3000);
 }
