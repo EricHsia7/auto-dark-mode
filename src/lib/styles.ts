eb6ba6176d55559201a02d37b9d41baf348705df
Eric <54091924+EricHsia7@users.noreply.github.com>
2025-06-06 14:03:21 +0800

    update

--- src/lib/styles.ts
+++ src/lib/styles.ts
@@ -203,6 +203,9 @@ export function getStyles() {
   // Capture all inline styles (lambda styles)
   function generateElementSelector(element: HTMLElement): string {
     const tag = element.tagName.toLowerCase();
+    if (!element.id) {
+      element.id = `_${generateIdentifier()}`;
+    }
     const id = element.id ? `#${element.id}` : '';
     const classes = element.classList.length > 0 ? `.${Array.from(element.classList).join('.')}` : '';
     return `${tag}${id}${classes}`;
