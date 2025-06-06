0c1946a52f5ec3c78b4c1b157faff8fc32d56dca
Eric <54091924+EricHsia7@users.noreply.github.com>
2025-06-06 19:21:38 +0800

    update

--- src/initialize.ts
+++ src/initialize.ts
@@ -2,10 +2,12 @@ import { generateIdentifier } from './lib/generate-identifier';
 import { getStyles, invertStyles, stylesToStrings } from './lib/styles';
 
 export function initialize(): void {
+  // Add transition
   const identifier = `_${generateIdentifier()}`;
   const transitionStyleTag = document.createElement('style');
   transitionStyleTag.textContent = '* {transition: color 0.5s ease, background-color 0.5s ease, border-top-color 0.5s ease, border-left-color 0.5s ease, border-right-color 0.5s ease, border-bottom-color 0.5s ease;}';
   transitionStyleTag.id = identifier;
+  document.documentElement.appendChild(transitionStyleTag);
 
   // Extract styles
   const styles = getStyles();
@@ -25,6 +27,7 @@ export function initialize(): void {
   }
   document.documentElement.appendChild(fragment);
 
+  // Remove transition
   setTimeout(function () {
     document.querySelector(`#${identifier}`)?.remove();
   }, 1000);
