9a7676a80dd099b09e8b75182f7c7c1d900da521
Eric <54091924+EricHsia7@users.noreply.github.com>
2025-06-08 03:33:31 +0800

    update

--- src/interface/control-panel/index.ts
+++ src/interface/control-panel/index.ts
@@ -59,11 +59,8 @@ export function initializeControlPanel(stylesStrings): void {
     ((stylesheetToggle2) => {
       stylesheetToggle2.addEventListener('click', function () {
         const currentState = stylesheetToggle2.getAttribute('state');
-        console.log(currentState);
         const nameValue = stylesheetToggle2.getAttribute('name');
-        console.log(0, nameValue);
         const styleTag = document.querySelector(`style[auto-dark-mode-stylesheet-name="${nameValue}"]`) as HTMLStyleElement;
-        console.log(1, styleTag);
         if (currentState === 'on') {
           stylesheetToggle2.setAttribute('state', 'off');
           styleTag.disabled = true;
