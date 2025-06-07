a2a9d94e32a68ce5de61bf8e9cf73b11423527bb
Eric <54091924+EricHsia7@users.noreply.github.com>
2025-06-08 00:23:09 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import { initialize } from './initialize';
 
+import './interface/control-panel/theme.css';
+import './interface/control-panel/mask.css';
+import './interface/control-panel/panel.css';
+import './interface/control-panel/head.css';
+import './interface/control-panel/body.css';
+import './interface/control-panel/stylesheets.css';
+
 var autoDarkMode = {
   initialize
 };
