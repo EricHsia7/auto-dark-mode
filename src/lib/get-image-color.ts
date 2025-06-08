d2040135b649d1055808483a4cdcdd7275dcd4c6
Eric <54091924+EricHsia7@users.noreply.github.com>
2025-06-08 23:26:16 +0800

    update

--- src/lib/get-image-color.ts
+++ src/lib/get-image-color.ts
@@ -8,25 +8,33 @@ export async function getImageColor(url: string): Promise<ParsedColorRGBA | fals
       const img = new Image();
       img.onload = function () {
         const colorThief = new ColorThief();
-        const colors = colorThief.getPalette(img, 3);
-        const colorsQuantity = colors.length;
-        let totalRed = 0;
-        let totalGreen = 0;
-        let totalBlue = 0;
-        for (const color of colors) {
-          totalRed += color[0];
-          totalGreen += color[1];
-          totalBlue += color[2];
-        }
-        const r = Math.floor(totalRed / colorsQuantity);
-        const g = Math.floor(totalGreen / colorsQuantity);
-        const b = Math.floor(totalBlue / colorsQuantity);
+        const colors = colorThief.getPalette(img, 5);
+        if (Array.isArray(colors) && typeof colors === 'object') {
+          const colorsQuantity = colors.length;
+          if (colorsQuantity > 0) {
+            let totalRed = 0;
+            let totalGreen = 0;
+            let totalBlue = 0;
+            for (const color of colors) {
+              totalRed += color[0];
+              totalGreen += color[1];
+              totalBlue += color[2];
+            }
+            const r = Math.floor(totalRed / colorsQuantity);
+            const g = Math.floor(totalGreen / colorsQuantity);
+            const b = Math.floor(totalBlue / colorsQuantity);
 
-        const result: ParsedColorRGBA = {
-          type: 'rgba',
-          rgba: [r, g, b, 1]
-        };
-        resolve(result);
+            const result: ParsedColorRGBA = {
+              type: 'rgba',
+              rgba: [r, g, b, 1]
+            };
+            resolve(result);
+          } else {
+            resolve(false);
+          }
+        } else {
+          resolve(false);
+        }
       };
 
       img.src = url;
--- src/lib/parse-color.ts
+++ src/lib/parse-color.ts
@@ -292,15 +292,6 @@ export async function parseColor(value: string): Promise<ParsedColor> {
     const urlMatch = value.match(/url\(\s*(['"]?)((?:https?:\/\/|data:|\.{0,2}\/|\/)[^'"()\s]+?)\1\s*\)/i);
     if (urlMatch !== null) {
       const url = urlMatch[2];
-      /*
-      let fullURL = ''
-      if(url.startsWith('http') || url.startsWith('data')) {
-        fullURL = url
-      }
-      else {
-        fullURL = resolveRelativePath(url);
-      }
-      */
       const color = await getImageColor(url);
       const coloredResult: ParsedColorURL = {
         type: 'url',
