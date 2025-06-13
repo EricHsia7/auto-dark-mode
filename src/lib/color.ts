a8dc6dffc3a8ee63cc3fbd64d0fde46b41cc0b99
Eric <54091924+EricHsia7@users.noreply.github.com>
2025-06-13 23:29:36 +0800

    update

--- src/lib/color.ts
+++ src/lib/color.ts
@@ -753,7 +753,7 @@ export function isColorDark(color: ColorRGBA): number {
 
 export function isColorVibrant(color: ColorRGB): number {
   // const p = 0.00526701208730907 * Math.abs(color.rgb[0] - color.rgb[1]) + 0.2315923467761 + 0.005789762029929 * Math.abs(color.rgb[1] - color.rgb[2]) + 0.268849448143961 + 0.00445659947538687 * Math.abs(color.rgb[0] - color.rgb[2]) + 0.20534779494441;
-  const p = 0.006389091699 * Math.abs(color.rgb[0] - color.rgb[1]) + 0.08927966221 + 0.007252694264 * Math.abs(color.rgb[1] - color.rgb[2]) + 0.1210052419 + 0.005242130442 * Math.abs(color.rgb[0] - color.rgb[2]) + 0.07115546734;
+  const p = 0.006454343571 * Math.abs(color.rgb[0] - color.rgb[1]) + 0.07863027694 + 0.007319708657 * Math.abs(color.rgb[1] - color.rgb[2]) + 0.1110610874 + 0.005285882122 * Math.abs(color.rgb[0] - color.rgb[2]) + 0.06090111834;
   const q = Math.min(Math.max(p / 3, 0), 1);
   return q;
   // higher number means higher probability
