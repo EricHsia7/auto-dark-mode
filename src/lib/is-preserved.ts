0b75fe8daa547e2012e5d099ee0ca6f08caefc4a
Eric <54091924+EricHsia7@users.noreply.github.com>
2025-06-25 19:47:24 +0800

    update

--- src/lib/is-preserved.ts
+++ src/lib/is-preserved.ts
@@ -1,6 +1,15 @@
 const preservedProperties = {
   'filter': true,
-  '-webkit-filter': true
+  '-webkit-filter': true,
+  'background-attachment': true,
+  'background-blend-mode': true,
+  'background-clip': true,
+  'background-origin': true,
+  'background-position-x': true,
+  'background-position-y': true,
+  'background-position': true,
+  'background-repeat': true,
+  'background-size': true
 };
 
 export function isPreserved(property: string): boolean {
