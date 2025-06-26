cf665adf40c04fd1a942d2795f0053f0ff83a348
Eric <54091924+EricHsia7@users.noreply.github.com>
2025-06-26 11:22:08 +0000

    update

--- src/initialize.ts
+++ src/initialize.ts
@@ -46,46 +46,46 @@ export async function initialize() {
   // Listen to changes
   const observer = new MutationObserver((mutationList, observer) => {
     const now = new Date().getTime();
-    if (now - lastUpdateTime > 500) {
-      let shouldGetFullStyles = false;
-      let shouldGetPartialStyles = false;
+    // if (now - lastUpdateTime > 1) {
+    let shouldGetFullStyles = false;
+    let shouldGetPartialStyles = false;
 
-      for (const mutation of mutationList) {
-        if (mutation.type === 'childList') {
-          mutation.addedNodes.forEach((node) => {
-            if (node instanceof HTMLLinkElement || (node instanceof HTMLStyleElement && node.tagName.toLowerCase() === 'style' && !node.hasAttribute('auto-dark-mode-stylesheet-name'))) {
-              shouldGetFullStyles = true;
-            } else if (node instanceof HTMLElement) {
-              shouldGetPartialStyles = true;
-            }
-          });
-        } else if (mutation.type === 'attributes') {
-          if (mutation.attributeName === 'style') {
+    for (const mutation of mutationList) {
+      if (mutation.type === 'childList') {
+        mutation.addedNodes.forEach((node) => {
+          if (node instanceof HTMLLinkElement || (node instanceof HTMLStyleElement && node.tagName.toLowerCase() === 'style' && !node.hasAttribute('auto-dark-mode-stylesheet-name'))) {
+            shouldGetFullStyles = true;
+          } else if (node instanceof HTMLElement) {
             shouldGetPartialStyles = true;
           }
+        });
+      } else if (mutation.type === 'attributes') {
+        if (mutation.attributeName === 'style') {
+          shouldGetPartialStyles = true;
         }
       }
-
-      if (shouldGetFullStyles) {
-        lastUpdateTime = now;
-        // Extract full styles
-        const styles = getStyles();
-        const invertedStyles = invertStyles(styles.stylesCollection, styles.referenceMap) as StylesCollection;
-        const stylesheets = generateCssFromStyles(invertedStyles, false);
-        updateStylesheets(stylesheets);
-      } else if (shouldGetPartialStyles) {
-        lastUpdateTime = now;
-        // Extract partial styles
-        const styles = getPartialStyles(mutationList);
-        // Patch styles
-        const patchedStylesCollection = Object.assign(currentStyles.stylesCollection, styles.stylesCollection);
-        const patchedReferenceMap = Object.assign(currentStyles.referenceMap, styles.referenceMap);
-        currentStyles = { stylesCollection: patchedStylesCollection, referenceMap: patchedReferenceMap };
-        const invertedStyles = invertStyles(patchedStylesCollection, patchedReferenceMap) as StylesCollection;
-        const stylesheets = generateCssFromStyles(invertedStyles, false);
-        updateStylesheets(stylesheets);
-      }
     }
+
+    if (shouldGetFullStyles) {
+      lastUpdateTime = now;
+      // Extract full styles
+      const styles = getStyles();
+      const invertedStyles = invertStyles(styles.stylesCollection, styles.referenceMap) as StylesCollection;
+      const stylesheets = generateCssFromStyles(invertedStyles, false);
+      updateStylesheets(stylesheets);
+    } else if (shouldGetPartialStyles) {
+      lastUpdateTime = now;
+      // Extract partial styles
+      const styles = getPartialStyles(mutationList);
+      // Patch styles
+      const patchedStylesCollection = Object.assign(currentStyles.stylesCollection, styles.stylesCollection);
+      const patchedReferenceMap = Object.assign(currentStyles.referenceMap, styles.referenceMap);
+      currentStyles = { stylesCollection: patchedStylesCollection, referenceMap: patchedReferenceMap };
+      const invertedStyles = invertStyles(patchedStylesCollection, patchedReferenceMap) as StylesCollection;
+      const stylesheets = generateCssFromStyles(invertedStyles, false);
+      updateStylesheets(stylesheets);
+    }
+    // }
   });
 
   observer.observe(document.body, { attributes: true, childList: true, subtree: true });
