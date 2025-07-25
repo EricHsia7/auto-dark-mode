09841b863376d1dccbf60f3fdf9e2027de36c501
Eric <54091924+EricHsia7@users.noreply.github.com>
2025-07-25 23:15:11 +0800

    update

--- src/lib/syntax-tags.ts
+++ src/lib/syntax-tags.ts
@@ -11,121 +11,121 @@ export type SyntaxTag = 'number' | 'integer' | 'float' | 'length' | 'font' | 'di
 
 export type SyntaxTagSet = Set<SyntaxTag>;
 
-export function getSyntaxTags(object: _Object | any): SyntaxTagSet {
+export function getSyntaxTags(obj: _Object | any): SyntaxTagSet {
   const tags = new Set();
 
-  if (typeof object !== 'object') return tags;
-  if (Array.isArray(object)) return tags;
-  if (!object.hasOwnProperty('type')) return tags;
+  if (typeof obj !== 'object') return tags;
+  if (Array.isArray(obj)) return tags;
+  if (!obj.hasOwnProperty('type')) return tags;
 
-  if (object.type === 'integer' || object.type === 'float') {
+  if (obj.type === 'integer' || obj.type === 'float') {
     tags.add('number');
-    if (object.type === 'integer') {
+    if (obj.type === 'integer') {
       tags.add('integer');
-    } else if (object.type === 'float') {
+    } else if (obj.type === 'float') {
       tags.add('float');
     }
-    if (object.value === 0) {
+    if (obj.value === 0) {
       tags.add('length');
       tags.add('font');
       tags.add('distance');
       tags.add('absolute');
       tags.add('zero');
     }
-  } else if (object.type === 'integer-u' || object.type === 'float-u') {
-    if ('%' === object.unit) {
+  } else if (obj.type === 'integer-u' || obj.type === 'float-u') {
+    if ('%' === obj.unit) {
       tags.add('position');
       tags.add('length');
       tags.add('distance');
       tags.add('font');
       tags.add('relative');
       tags.add('percentage');
-    } else if (['px', 'pt', 'pc', 'cm', 'in', 'mm', 'Q'].indexOf(object.unit) > -1) {
+    } else if (['px', 'pt', 'pc', 'cm', 'in', 'mm', 'Q'].indexOf(obj.unit) > -1) {
       tags.add('length');
       tags.add('distance');
       tags.add('font');
       tags.add('absolute');
-    } else if (['deg', 'rad', 'turn', 'grad'].indexOf(object.unit) > -1) {
+    } else if (['deg', 'rad', 'turn', 'grad'].indexOf(obj.unit) > -1) {
       tags.add('angle');
-    } else if (['dvh', 'dvw', 'lvh', 'lvw', 'svh', 'svw', 'vb', 'vh', 'vi', 'vmax', 'vmin', 'vw'].indexOf(object.unit) > -1) {
+    } else if (['dvh', 'dvw', 'lvh', 'lvw', 'svh', 'svw', 'vb', 'vh', 'vi', 'vmax', 'vmin', 'vw'].indexOf(obj.unit) > -1) {
       tags.add('length');
       tags.add('viewport');
       tags.add('distance');
       tags.add('absolute');
-    } else if (['em', 'ch', 'cap', 'ic', 'lh'].indexOf(object.unit) > -1) {
+    } else if (['em', 'ch', 'cap', 'ic', 'lh'].indexOf(obj.unit) > -1) {
       tags.add('length');
       tags.add('font');
       tags.add('distance');
       tags.add('relative');
-    } else if (['rem', 'rch', 'rcap', 'ric', 'rlh'].indexOf(object.unit) > -1) {
+    } else if (['rem', 'rch', 'rcap', 'ric', 'rlh'].indexOf(obj.unit) > -1) {
       tags.add('length');
       tags.add('font');
       tags.add('distance');
       tags.add('relative');
-    } else if (['cqw', 'cqh', 'cqi', 'cqb', 'cqmin', 'cqmax'].indexOf(object.unit) > -1) {
+    } else if (['cqw', 'cqh', 'cqi', 'cqb', 'cqmin', 'cqmax'].indexOf(obj.unit) > -1) {
       tags.add('length');
       tags.add('distance');
       tags.add('container');
       tags.add('relative');
     }
-    if (object.type === 'integer-u') {
+    if (obj.type === 'integer-u') {
       tags.add('integer');
-    } else if (object.type === 'float-u') {
+    } else if (obj.type === 'float-u') {
       tags.add('float');
     }
-    if (object.value === 0) {
+    if (obj.value === 0) {
       tags.add('zero');
     }
-  } else if (object.type === 'string') {
-    if (/^#[a-f0-9]{3,8}/i.test(object.value)) {
+  } else if (obj.type === 'string') {
+    if (/^#[a-f0-9]{3,8}/i.test(obj.value)) {
       tags.add('hex');
       tags.add('color');
-    } else if ('to' === object.value) {
+    } else if ('to' === obj.value) {
       tags.add('preposition');
       tags.add('to');
-    } else if ('at' === object.value) {
+    } else if ('at' === obj.value) {
       tags.add('preposition');
       tags.add('at');
-    } else if ('from' === object.value) {
+    } else if ('from' === obj.value) {
       tags.add('preposition');
       tags.add('from');
-    } else if (['top', 'left', 'bottom', 'right'].indexOf(object.value) > -1) {
+    } else if (['top', 'left', 'bottom', 'right'].indexOf(obj.value) > -1) {
       tags.add('position');
       tags.add('cardinal');
-      if (['top', 'bottom'].indexOf(object.value) > -1) {
+      if (['top', 'bottom'].indexOf(obj.value) > -1) {
         tags.add('vertical');
-      } else if (['left', 'right'].indexOf(object.value) > -1) {
+      } else if (['left', 'right'].indexOf(obj.value) > -1) {
         tags.add('horizontal');
       }
-    } else if ('center' === object.value) {
+    } else if ('center' === obj.value) {
       tags.add('position');
       tags.add('vertical');
       tags.add('horizontal');
       tags.add('center');
-    } else if (['closest-side', 'closest-corner', 'farthest-side', 'farthest-corner'].indexOf(object.value) > -1) {
+    } else if (['closest-side', 'closest-corner', 'farthest-side', 'farthest-corner'].indexOf(obj.value) > -1) {
       tags.add('position');
       tags.add('extent');
-    } else if (['circle', 'ellipse'].indexOf(object.value) > -1) {
+    } else if (['circle', 'ellipse'].indexOf(obj.value) > -1) {
       tags.add('shape');
-    } else if (/^--[a-z0-9_\-]+$/i.test(object.value)) {
+    } else if (/^--[a-z0-9_\-]+$/i.test(obj.value)) {
       tags.add('variable-name');
-    } else if (namedColors.hasOwnProperty(object.value)) {
+    } else if (namedColors.hasOwnProperty(obj.value)) {
       tags.add('named-color');
       tags.add('color');
-    } else if (systemColors.hasOwnProperty(object.value)) {
+    } else if (systemColors.hasOwnProperty(obj.value)) {
       tags.add('system-color');
       tags.add('color');
-    } else if (functionalKeywords.hasOwnProperty(object.value)) {
+    } else if (functionalKeywords.hasOwnProperty(obj.value)) {
       tags.add('functional-keyword');
       tags.add('color');
     }
-  } else if (object.type === 'model') {
+  } else if (obj.type === 'model') {
     tags.add('model');
-    if (object.model === 'var') {
+    if (obj.model === 'var') {
       tags.add('variable');
-    } else if (['rgb', 'rgba', 'hsl', 'hsla'].indexOf(object.model) > -1) {
+    } else if (['rgb', 'rgba', 'hsl', 'hsla'].indexOf(obj.model) > -1) {
       tags.add('color');
-    } else if (['linear-gradient', 'radial-gradient', 'conic-gradient'].indexOf(object.model) > -1) {
+    } else if (['linear-gradient', 'radial-gradient', 'conic-gradient'].indexOf(obj.model) > -1) {
       tags.add('gradient');
     }
   }
