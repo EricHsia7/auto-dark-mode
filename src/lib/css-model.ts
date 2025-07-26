0b6fbc93bc91285d7071def073ceada423e5653e
Eric <54091924+EricHsia7@users.noreply.github.com>
2025-07-26 23:26:55 +0800

    update

---
+++ src/lib/css-model.ts
@@ -0,0 +1,15 @@
+export type CSSLinearGradient = 'linear-gradient';
+export type CSSRadialGradient = 'radial-gradient';
+export type CSSConicGradient = 'conic-gradient';
+
+export type CSSGradient = CSSLinearGradient | CSSRadialGradient | CSSConicGradient;
+
+export type CSSRGB = 'rgb';
+export type CSSRGBA = 'rgba';
+export type CSSHSL = 'hsl';
+export type CSSHSLA = 'hsla';
+export type CSSColorMix = 'color-mix';
+
+export type CSSColor = CSSRGB | CSSRGBA | CSSHSL | CSSHSLA | CSSColorMix;
+
+export type CSSCalc = 'calc';
--- src/lib/css-position.ts
+++ src/lib/css-position.ts
@@ -4,6 +4,7 @@ import { isPercentage } from './css-units';
 export type CSSPrepositionFrom = 'from';
 export type CSSPrepositionTo = 'to';
 export type CSSPrepositionAt = 'at';
+
 export type CSSPreposition = CSSPrepositionFrom | CSSPrepositionTo | CSSPrepositionAt;
 
 export type CSSPositionTop = 'top';
@@ -11,19 +12,32 @@ export type CSSPositionLeft = 'left';
 export type CSSPositionBottom = 'bottom';
 export type CSSPositionRight = 'right';
 
-export type CSSPositionCenter = 'center';
+export type CSSCardinalPosition = CSSPositionTop | CSSPositionLeft | CSSPositionBottom | CSSPositionRight;
 
 export type CSSPositionClosestSide = 'closest-side';
 export type CSSPositionClosestCorner = 'closest-corner';
 export type CSSPositionFarthestSide = 'farthest-side';
 export type CSSPositionFarthestCorner = 'farthest-corner';
 
-export type CSSCardinalPosition = CSSPositionTop | CSSPositionLeft | CSSPositionBottom | CSSPositionRight;
 export type CSSExtentPosition = CSSPositionClosestSide | CSSPositionClosestCorner | CSSPositionFarthestSide | CSSPositionFarthestCorner;
+
+export type CSSPositionCenter = 'center';
 
 export const CSSCardinalPositions: Array<CSSCardinalPosition> = ['top', 'left', 'bottom', 'right'];
 
 export const CSSExtentPositions: Array<CSSCardinalPosition> = ['closest-side', 'closest-corner', 'farthest-side', 'farthest-corner'];
+
+export function isFromPreposition(stringComponent: StringComponent): boolean {
+  return stringComponent.string === 'from';
+}
+
+export function isToPreposition(stringComponent: StringComponent): boolean {
+  return stringComponent.string === 'to';
+}
+
+export function isAtPreposition(stringComponent: StringComponent): boolean {
+  return stringComponent.string === 'at';
+}
 
 export function isCardinalPosition(stringComponent: StringComponent): boolean {
   return CSSCardinalPositions.indexOf(stringComponent.string) > -1;
