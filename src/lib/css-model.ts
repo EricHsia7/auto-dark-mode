99b334b9627f35e660acaac30ee0384debefd978
Eric <54091924+EricHsia7@users.noreply.github.com>
2025-07-26 23:40:23 +0800

    update

--- src/lib/css-model.ts
+++ src/lib/css-model.ts
@@ -14,16 +14,22 @@ export type CSSColorMix = 'color-mix';
 
 export type CSSColor = CSSRGB | CSSRGBA | CSSHSL | CSSHSLA | CSSColorMix;
 
+export type CSSVAR = 'var';
+
 export type CSSCalc = 'calc';
 
 export const CSSColors: Array<CSSColor> = ['rgb', 'rgba', 'hsl', 'hsla', 'color-mix'];
 
 export const CSSGradients: Array<CSSGradient> = ['linear-gradient', 'radial-gradient', 'conic-gradient'];
 
-export function isColor(modelComponent: ModelComponent<string>): boolean {
-  return CSSColors.indexOf(modelComponent.model) > -1;
+export function isColor(modelComponent: ModelComponent<string>): modelComponent is ModelComponent<CSSColor> {
+  return CSSColors.indexOf(modelComponent.model as CSSColor) > -1;
 }
 
-export function isGradient(modelComponent: ModelComponent<string>): boolean {
-  return CSSGradients.indexOf(modelComponent.model) > -1;
+export function isGradient(modelComponent: ModelComponent<string>): modelComponent is ModelComponent<CSSGradient> {
+  return CSSGradients.indexOf(modelComponent.model as CSSGradient) > -1;
+}
+
+export function isVariable(modelComponent: ModelComponent<string>): modelComponent is ModelComponent<CSSVAR> {
+  return (modelComponent.model as CSSVAR) === 'var';
 }
