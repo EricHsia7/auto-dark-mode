6d12bfe226232ef60eaac757650b237150553275
Eric <54091924+EricHsia7@users.noreply.github.com>
2025-07-26 23:34:33 +0800

    update

--- src/lib/css-model.ts
+++ src/lib/css-model.ts
@@ -1,3 +1,5 @@
+import { ModelComponent } from './component';
+
 export type CSSLinearGradient = 'linear-gradient';
 export type CSSRadialGradient = 'radial-gradient';
 export type CSSConicGradient = 'conic-gradient';
@@ -13,3 +15,15 @@ export type CSSColorMix = 'color-mix';
 export type CSSColor = CSSRGB | CSSRGBA | CSSHSL | CSSHSLA | CSSColorMix;
 
 export type CSSCalc = 'calc';
+
+export const CSSColors: Array<CSSColor> = ['rgb', 'rgba', 'hsl', 'hsla', 'color-mix'];
+
+export const CSSGradients: Array<CSSGradient> = ['linear-gradient', 'radial-gradient', 'conic-gradient'];
+
+export function isColor(modelComponent: ModelComponent<string>): boolean {
+  return CSSColors.indexOf(modelComponent.model) > -1;
+}
+
+export function isGradient(modelComponent: ModelComponent<string>): boolean {
+  return CSSGradients.indexOf(modelComponent.model) > -1;
+}
