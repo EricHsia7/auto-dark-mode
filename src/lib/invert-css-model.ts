157e2357d376a9a91b021f50895cf947b283d7d1
Eric <54091924+EricHsia7@users.noreply.github.com>
2025-07-27 11:25:35 +0800

    update

--- src/lib/invert-css-model.ts
+++ src/lib/invert-css-model.ts
@@ -1,5 +1,6 @@
 import { ModelComponent } from './component';
 import { CSSColor, CSSRGB, CSSRGBA } from './css-model';
+import { hslToRgb } from './hsl-to-rgb';
 import { invertColor } from './invert-color';
 
 function invertCSSModel(modelComponent: ModelComponent<CSSColor>, darkened: boolean = false): ModelComponent<CSSColor> {
@@ -11,7 +12,53 @@ function invertCSSModel(modelComponent: ModelComponent<CSSColor>, darkened: bool
 
       const [R, G, B] = invertColor(red.number, green.number, blue.number, darkened);
 
-      if (alpha !== undefined && alpha.type === 'number') {
+      if (alpha === undefined) {
+        const result: ModelComponent<CSSRGB> = {
+          type: 'model',
+          model: 'rgb',
+          components: [
+            { type: 'number', number: R, unit: '' },
+            { type: 'number', number: G, unit: '' },
+            { type: 'number', number: B, unit: '' }
+          ]
+        };
+        return result;
+      } else if (alpha.type === 'number' && alpha.number === 1) {
+        const result: ModelComponent<CSSRGB> = {
+          type: 'model',
+          model: 'rgb',
+          components: [
+            { type: 'number', number: R, unit: '' },
+            { type: 'number', number: G, unit: '' },
+            { type: 'number', number: B, unit: '' }
+          ]
+        };
+        return result;
+      } else if (alpha.type === 'number' && alpha.unit === '') {
+        const result: ModelComponent<CSSRGBA> = {
+          type: 'model',
+          model: 'rgba',
+          components: [{ type: 'number', number: R, unit: '' }, { type: 'number', number: G, unit: '' }, { type: 'number', number: B, unit: '' }, alpha]
+        };
+        return result;
+      } else {
+        const result: ModelComponent<CSSRGBA> = {
+          type: 'model',
+          model: 'rgba',
+          components: [{ type: 'number', number: R, unit: '' }, { type: 'number', number: G, unit: '' }, { type: 'number', number: B, unit: '' }, alpha]
+        };
+        return result;
+      }
+    }
+
+    case 'rgba': {
+      const [red, green, blue, alpha] = modelComponent.components;
+
+      if (red.type !== 'number' || green.type !== 'number' || blue.type !== 'number' || alpha === undefined) return modelComponent;
+
+      const [R, G, B] = invertColor(red.number, green.number, blue.number, darkened);
+
+      if (alpha.type === 'number') {
         if (alpha.number === 1) {
           const result: ModelComponent<CSSRGB> = {
             type: 'model',
@@ -37,19 +84,67 @@ function invertCSSModel(modelComponent: ModelComponent<CSSColor>, darkened: bool
           return result;
         }
       } else {
-        const result: ModelComponent<CSSRGB> = {
+        const result: ModelComponent<CSSRGBA> = {
           type: 'model',
-          model: 'rgb',
+          model: 'rgba',
           components: [
             { type: 'number', number: R, unit: '' },
             { type: 'number', number: G, unit: '' },
-            { type: 'number', number: B, unit: '' }
+            { type: 'number', number: B, unit: '' },
+            { type: 'number', number: alpha, unit: '' }
           ]
         };
         return result;
       }
+    }
 
-      break;
+    case 'hsl': {
+      const [hue, saturation, lightness, alpha] = modelComponent.components;
+
+      if (hue.type !== 'number' || saturation.type !== 'number' || lightness.type !== 'number') return modelComponent;
+      if (hue.unit !== '' || saturation.unit !== '%' || lightness.unit !== '%') return modelComponent;
+
+      const [R, G, B] = hslToRgb(hue, saturation, lightness);
+      const [R1, G1, B1] = invertColor(R, G, B, darkened);
+
+      if (alpha.type === 'number') {
+        if (alpha.number === 1) {
+          const result: ModelComponent<CSSRGB> = {
+            type: 'model',
+            model: 'rgb',
+            components: [
+              { type: 'number', number: R1, unit: '' },
+              { type: 'number', number: G1, unit: '' },
+              { type: 'number', number: B1, unit: '' }
+            ]
+          };
+          return result;
+        } else {
+          const result: ModelComponent<CSSRGBA> = {
+            type: 'model',
+            model: 'rgba',
+            components: [
+              { type: 'number', number: R1, unit: '' },
+              { type: 'number', number: G1, unit: '' },
+              { type: 'number', number: B1, unit: '' },
+              { type: 'number', number: alpha, unit: '' }
+            ]
+          };
+          return result;
+        }
+      } else {
+        const result: ModelComponent<CSSRGBA> = {
+          type: 'model',
+          model: 'rgba',
+          components: [
+            { type: 'number', number: R, unit: '' },
+            { type: 'number', number: G, unit: '' },
+            { type: 'number', number: B, unit: '' },
+            { type: 'number', number: alpha, unit: '' }
+          ]
+        };
+        return result;
+      }
     }
 
     default:
