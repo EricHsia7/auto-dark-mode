5a2a36b9c35ce30d85b782e59b951c2d5c32eac8
Eric <54091924+EricHsia7@users.noreply.github.com>
2025-07-27 22:30:58 +0800

    update

--- src/lib/extract-rgba.ts
+++ src/lib/extract-rgba.ts
@@ -2,12 +2,21 @@ import { ModelComponent } from './component';
 import { CSSColor, CSSGradient, CSSVAR } from './css-model';
 
 export function extractRGBA(modelComponent: ModelComponent<CSSColor | CSSVAR | CSSGradient>): [red: number, green: number, blue: number, alpha: number] | undefined {
-switch (modelComponent.model) {
-  case "":
-    
-    break;
+  switch (modelComponent.model) {
+    case 'rgb': {
+      const [red, green, blue, alpha] = modelComponent.components;
 
-  default:
-    break;
-}
+      if (typeof red !== 'object' || typeof green !== 'object' || typeof blue !== 'object') return [0, 0, 0, 0];
+      if (red?.type !== 'number' || green?.type !== 'number' || blue?.type !== 'number') return [0, 0, 0, 0];
+
+      if (alpha === undefined) {
+        return [red.number, green.number, blue.number, 1];
+      } else if (alpha.type === 'number') {
+        return [red.number, green.number, blue.number, alpha.number];
+      }
+      break;
+    }
+    default:
+      break;
+  }
 }
