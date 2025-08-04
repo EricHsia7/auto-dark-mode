249eddbdf605d136fd0aaf5ca8c1a3fdb51c3a8c
Eric <54091924+EricHsia7@users.noreply.github.com>
2025-08-04 22:57:40 +0800

    update

--- src/lib/spread-css-variables.ts
+++ src/lib/spread-css-variables.ts
@@ -1,5 +1,5 @@
-import { Component, ModelComponent } from './component';
-import { CSSColor, CSSGradient, CSSVAR } from './css-model';
+import { Component, ModelComponent, parseComponent } from './component';
+import { CSSColor, CSSGradient, CSSVAR, isColor, isGradient, isVariable, parseCSSModel } from './css-model';
 import { isPathContinuous } from './is-path-continuous';
 import { splitByTopLevelDelimiter } from './split-by-top-level-delimiter';
 
@@ -28,19 +28,31 @@ function getSpreadComponents(variableComponent: ModelComponent<CSSVAR>, variable
         const args = splitByTopLevelDelimiter(value).result;
         const argsLen = args.length;
         for (let j = argsLen - 1; j >= 0; j--) {
-          const property = `--varlib-${component.string}-${j.toString()}`;
-          // TODO: check sub property length
-          const spreadComponent: ModelComponent<CSSVAR> = {
-            type: 'model',
-            model: 'var',
-            components: [
-              {
-                type: 'string',
-                string: property
-              }
-            ]
-          };
-          spreadComponents.unshift(spreadComponent);
+          const arg = args[j];
+          const parsed = parseCSSModel(arg) || parseComponent(arg);
+          if (parsed !== undefined) {
+            if (parsed.type === 'model') {
+              // spread variables in intermediate models
+              if (isColor(parsed) || isGradient(parsed)) {
+                spreadComponents.unshift(spreadCSSVariables(parsed, variableLibrary, mediaQueryConditionsText, selectorText));
+              } else if (isVariable(parsed)) {
+                spreadComponents.unshift.apply(getSpreadComponents(component, variableLibrary, mediaQueryConditionsText, selectorText));
+              } 
+            } else {
+              const property = `--varlib-${component.string}-${j.toString()}`;
+              const spreadComponent: ModelComponent<CSSVAR> = {
+                type: 'model',
+                model: 'var',
+                components: [
+                  {
+                    type: 'string',
+                    string: property
+                  }
+                ]
+              };
+              spreadComponents.unshift(spreadComponent);
+            }
+          }
         }
         break;
       }
