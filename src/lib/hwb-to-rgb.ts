4d0b47e8b23561282065bc99aacce070b81802f6
Eric <54091924+EricHsia7@users.noreply.github.com>
2025-08-05 19:07:39 +0800

    update

--- src/lib/hwb-to-rgb.ts
+++ src/lib/hwb-to-rgb.ts
@@ -21,7 +21,6 @@ export function hwbToRgb(hue: number, white: number, black: number): [red: numbe
 
 export function getConvertedHWBCSSVariables(id: string, hue: Component, white: Component, black: Component, container: CSSProperties): [red: ModelComponent<CSSVAR>, green: ModelComponent<CSSVAR>, blue: ModelComponent<CSSVAR>] {
   const baseName = `--${id}-hwb`;
-  const H = `calc(${stringifyComponent(hue, cssPrimaryDelimiters)} / 1deg)`;
   const W = `calc(${stringifyComponent(white, cssPrimaryDelimiters)} / 100%)`;
   const B = `calc(${stringifyComponent(black, cssPrimaryDelimiters)} / 100%)`;
   container[`${baseName}-wb`] = `calc(${W} + ${B})`;
@@ -29,7 +28,7 @@ export function getConvertedHWBCSSVariables(id: string, hue: Component, white: C
   container[`${baseName}-gray`] = `calc(${W} / var(${baseName}-wb))`;
   container[`${baseName}-x`] = `calc(1 - var(${baseName}-wb))`;
   container[`${baseName}-y`] = `calc(${W} * 255)`;
-  const [r, g, b] = getConvertedHSLCSSVariables(id, H, { type: 'number', number: 100, unit: '%' }, { type: 'number', number: 50, unit: '%' }, container);
+  const [r, g, b] = getConvertedHSLCSSVariables(id, { type: 'model', model: 'calc', components: [{ type: 'string', string: `${stringifyComponent(hue, cssPrimaryDelimiters)} / 1deg` }] }, { type: 'number', number: 100, unit: '%' }, { type: 'number', number: 50, unit: '%' }, container);
   container[`${baseName}-r`] = `round(clamp(0,calc(var(${baseName}-x) * ${stringifyComponent(r, cssPrimaryDelimiters)} + var(${baseName}-y)),255))`;
   container[`${baseName}-g`] = `round(clamp(0,alc(var(${baseName}-x) * ${stringifyComponent(g, cssPrimaryDelimiters)} + var(${baseName}-y)),255))`;
   container[`${baseName}-b`] = `round(clamp(0,calc(var(${baseName}-x) * ${stringifyComponent(b, cssPrimaryDelimiters)} + var(${baseName}-y)),255))`;
