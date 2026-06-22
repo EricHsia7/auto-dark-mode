b121db2afce3c19bd275c3f88ee16e494e7f903f
Eric <54091924+EricHsia7@users.noreply.github.com>
2026-06-23 01:51:46 +0800

    update

--- src/lib/is-color-dark.ts
+++ src/lib/is-color-dark.ts
@@ -3,7 +3,7 @@ import { sigmoid } from './sigmoid';
 
 export function isColorDark(red: number, green: number, blue: number, alpha: number | undefined = 1): number {
   if (alpha === undefined) alpha = 1;
-  const p = sigmoid(9.733 - 0.0207 * red - 0.01568 * green - 0.04519 * blue);
+  const p = sigmoid(9.733 - 0.0207 * red - 0.01568 * green - 0.04519 * blue); // logistic regression
   const q = clamp(0, p * alpha, 1);
   // ./data/darkness.csv
   return q;
