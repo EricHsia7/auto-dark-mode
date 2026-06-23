e80005e6a58d6a90e1e071baa6fdde60ed945aee
Eric <54091924+EricHsia7@users.noreply.github.com>
2026-06-23 09:29:45 +0800

    update

--- src/lib/is-color-dark.ts
+++ src/lib/is-color-dark.ts
@@ -4,8 +4,8 @@ import { sigmoid } from './sigmoid';
 export function isColorDark(red: number, green: number, blue: number, alpha: number | undefined = 1): number {
   if (alpha === undefined) alpha = 1;
   const p = sigmoid(9.733 - 0.0207 * red - 0.01568 * green - 0.04519 * blue); // logistic regression
-  const q = clamp(0, p * alpha, 1);
+  const q = sigmoid(9.19 * (alpha - 0.5)); // 2ln(99) ≈ 9.19
   // ./data/darkness.csv
-  return q;
+  return p * q;
   // higher number means higher probability
 }
